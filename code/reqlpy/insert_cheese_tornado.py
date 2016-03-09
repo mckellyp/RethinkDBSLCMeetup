62d043f0d47d6585be791e632173d69447f57d0e
Peart <mpeart@cscinfo.com>
2016-03-09 01:01:29 -0700

    Cleaning up the js, some other tweaks.

--- code/reqlpy/insert_cheese_tornado.py
+++ code/reqlpy/insert_cheese_tornado.py
@@ -9,6 +9,7 @@ conn = r.connect(host='50.116.0.34', port=28015, db='CheeseTornado')
 # Insert some records into the table
 r.table('Cheese').insert([
     {'flavor': 'Yellow',
-     'status': 'Moved'
+     'status': 'Moved',
+     'color' : 'Blue'
      }
 ]).run(conn)
--- code/rethinkdb-tornado/public/main.js
+++ code/rethinkdb-tornado/public/main.js
@@ -13,8 +13,10 @@ Handlebars.registerHelper('truncate', function(str, len) {
   return str;
 });
 
+/*
 //Create the button template
 var buttonTemplate = Handlebars.compile($("#button-template").html());
+
 
 //On enter in the tag entry form send the tag to be added as a subscription
 $("#tag").on('keyup', function(e) {
@@ -64,64 +66,7 @@ var removeTag = function(e) {
 
 //Add event to already generated HTML 
 $("#hashtag_list").on("click", "button", removeTag);
-
-
-// //Create the websocket to the location host
-// var ws = new WebSocket('ws://' + window.location.host + '/ws');
-
-// //Compile the template
-// var source = $("#gram-template").html();
-// var template = Handlebars.compile(source);
-
-// //Save the #main element since it will be accessed frequently
-// var main = $("#main");
-
-// //Create a queue of new posts to add to the page every second.
-// var gram_queue = []
-
-// ws.onmessage = function(ev) {
-
-//   //Parse the JSON received from the server
-//   var json = JSON.parse(ev.data);
-
-//   //Preload the image
-//   var i = new Image();
-//   i.src = json.new_val.images.low_resolution.url;
-
-//   //Render the template
-//   var html = template(json);
-
-//   gram_queue.push(html);
-// };
-
-// window.setInterval(function() {
-
-//   if (gram_queue.length > 0) {
-
-//     if (main.find(".row:first").children().length == 3) {
-//       main.prepend("<div class='row'></div>");
-//     }
-
-//     //Get the front item off the gram queue
-//     var rendered_gram = gram_queue.shift();
-
-//     //Append the new gram
-//     var new_gram = main.find(".row:first").append(rendered_gram);
-
-//     //Fade it in
-//     new_gram.find(".gram:last").transition({
-//       "opacity": 100
-//     });
-
-//     //Clean up the list
-//     var rows = $("#main .row");
-//     if (rows.length > 8) {
-//       console.log("Cleaning up rows you cannot see!");
-//       rows.slice(8).remove();
-//     }
-//   }
-// }, 600);
-
+*/
 
 var ws = new WebSocket('ws://' + window.location.host + '/ws');
 
@@ -143,7 +88,7 @@ ws.onmessage = function(ev) {
   console.log(json);
 
   //Render the template
-  var html = template(json.new_val);
+  var html = template(json);
 
   gram_queue.push(html);
   console.log(gram_queue.length);
@@ -153,7 +98,7 @@ window.setInterval(function() {
 
   if (gram_queue.length > 0) {
 
-    if ( main.find(".row:first").length == 0 || main.find(".row:first").children().length == 3) {
+    if ( main.find(".row:first").length == 0 || main.find(".row:first").children().length == 8) {
       main.prepend("<div class='row'></div>");
     }
 
